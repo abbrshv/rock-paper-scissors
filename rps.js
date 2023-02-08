8dc7e5acb1803e02300228c4e300315670c8cc6e
Anatoliy Bobryshov <a.bobryshov@proton.me>
2023-02-08 15:55:33 +0200

    Implement playRound

--- rps.js
+++ rps.js
@@ -14,3 +14,29 @@ function getPlayerChoice() {
 
   return playerSelection;
 }
+
+function playRound(playerSelection, computerSelection) {
+  let result =
+    choices.indexOf(playerSelection) - choices.indexOf(computerSelection);
+
+  switch (result) {
+    case 0:
+      console.log("Tie  !");
+      return playRound(getPlayerChoice(), getComputerChoice());
+    case -1:
+    case 2:
+      console.log(
+        `You lose! ${
+          computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)
+        } beats ${playerSelection}.`
+      );
+      return false;
+    default:
+      console.log(
+        `You win! ${
+          playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)
+        } beats ${computerSelection}.`
+      );
+      return true;
+  }
+}
