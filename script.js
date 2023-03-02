e9e47a7f370675ce1096eb0f93b1a03bac782742
abbrshv <a.bobryshov@proton.me>
2023-03-03 00:27:07 +0200

    Add css styling

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const playerScoreDisplay = document.querySelector(".score-player");
-const computerScoreDisplay = document.querySelector(".score-computer");
+const roundMessage = document.querySelector(".round-text");
+const playerScoreDisplay = document.querySelector(".score-player-num");
+const computerScoreDisplay = document.querySelector(".score-computer-num");
 const choiceButtons = document.querySelectorAll(".btn-choice");
 const endgameModal = document.querySelector(".endgame-modal");
 const endgameText = document.querySelector(".endgame-text");
@@ -8,20 +9,23 @@ const restartButton = document.querySelector(".btn-restart");
 const choices = ["ROCK", "PAPER", "SCISSORS"];
 let playerScore = 0;
 let computerScore = 0;
+let message;
 
 function getComputerChoice() {
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
 function updateScoreDisplay() {
-  playerScoreDisplay.textContent = `Player: ${playerScore}`;
-  computerScoreDisplay.textContent = `Computer: ${computerScore}`;
+  playerScoreDisplay.textContent = playerScore;
+  computerScoreDisplay.textContent = computerScore;
+  roundMessage.textContent = message;
 }
 
 function restartGame() {
   endgameModal.style.display = "none";
   playerScore = 0;
   computerScore = 0;
+  message = '';
   updateScoreDisplay();
 }
 
@@ -31,23 +35,20 @@ function playRound(playerSelection, computerSelection) {
 
   switch (result) {
     case 0:
-      console.log("Tie!");
+      message = "Tie!";
+      updateScoreDisplay();
       return;
     case -1:
     case 2:
-      console.log(
-        `You lose! ${
-          computerSelection.charAt(0) + computerSelection.slice(1).toLowerCase()
-        } beats ${playerSelection}.`
-      );
+      message = `You lose! ${
+        playerSelection.charAt(0) + playerSelection.slice(1).toLowerCase()
+      } is beaten by ${computerSelection.toLowerCase()}.`;
       computerScore++;
       break;
     default:
-      console.log(
-        `You win! ${
-          playerSelection.charAt(0) + playerSelection.slice(1).toLowerCase()
-        } beats ${computerSelection}.`
-      );
+      message = `You win! ${
+        playerSelection.charAt(0) + playerSelection.slice(1).toLowerCase()
+      } beats ${computerSelection.toLowerCase()}.`;
       playerScore++;
       break;
   }
@@ -56,8 +57,8 @@ function playRound(playerSelection, computerSelection) {
 
   if (playerScore === 5 || computerScore === 5) {
     endgameModal.style.display = "block";
-    endgameText.textContent = `You ${playerScore === 5 ? "Win" : "Lose"}!`
-    restartButton.addEventListener('click', restartGame);
+    endgameText.textContent = `You ${playerScore === 5 ? "Win" : "Lose"}!`;
+    restartButton.addEventListener("click", restartGame);
   }
 }
 
